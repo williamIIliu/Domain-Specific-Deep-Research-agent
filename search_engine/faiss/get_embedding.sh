66f39fcbba5fcbd0f2dde94084ab0ff453d23e10
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-27 20:41:36 +0800

    build_embedding

--- search_engine/faiss/get_embedding.sh
+++ search_engine/faiss/get_embedding.sh
@@ -4,6 +4,6 @@ python -m torch.distributed.run --nproc_per_node=1 \
   --model_path="./pretrain_models/embedding/Qwen3-Embedding-0.6B" \
   --save_dir="./datasets/database/data_with_embedding_shards" \
   --num_workers=2 \
-  --batch_size=1 \
+  --batch_size=64 \
   --max_length=1024 \
   --fp16
