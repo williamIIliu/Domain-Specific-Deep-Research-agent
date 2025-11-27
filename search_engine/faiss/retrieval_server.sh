c1b60b54f80b4da9fa42c520ee1036b797693925
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-28 00:46:43 +0800

    retrieval

--- search_engine/faiss/retrieval_server.sh
+++ search_engine/faiss/retrieval_server.sh
@@ -13,6 +13,5 @@
    --index_faiss_path "./datasets/database/faiss_qwen/faiss_index.bin" \
    --task_desc "根据给定的搜索查询，检索最相关的段落来回答问题" \
    --jsonl_path "./datasets/OmniEval-Corpus/all_data_clean.jsonl" \
-   --log_path "./logs/retrieval" \
    --embedding_model_path "./pretrain_models/embedding/Qwen3-Embedding-0.6B" \
    --retrieval_method "dense"
