f980d1b3086bb776a4c985e12544276c1c0c04bb
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-28 23:33:20 +0800

    post_train_scripts

--- verl/custom/run_qwen_05_sp2_liger.sh
+++ verl/custom/run_qwen_05_sp2_liger.sh
@@ -11,17 +11,17 @@ save_path=$2
 # Shift the arguments so $@ refers to the rest
 shift 2
 
-torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
+torchrun --standalone --nnodes=2 --nproc_per_node=$nproc_per_node \
      -m verl.trainer.fsdp_sft_trainer \
-    data.train_files=./datasets/Agentar-DeepFinance-100K/train.parquet \
-    data.val_files=./datasets/Agentar-DeepFinance-100K/test.parquet \
+    data.train_files=../datasets/Agentar-DeepFinance-100K/train.parquet \
+    data.val_files=../datasets/Agentar-DeepFinance-100K/test.parquet \
     data.prompt_key=question \
     data.response_key=answer \
     optim.lr=1e-4 \
     data.prompt_dict_keys=['question'] \
     +data.response_dict_keys=['answer'] \
     data.micro_batch_size=4 \
-    model.partial_pretrain=./pretrain_models/generator/Qwen3-8B \
+    model.partial_pretrain=../pretrain_models/generator/Qwen3-8B \
     model.use_liger=True \
     trainer.default_local_dir=$save_path \
     trainer.project_name=BDI \
@@ -29,4 +29,5 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
     trainer.logger=console $@ \
     ulysses_sequence_parallel_size=2 \
     use_remove_padding=true \
-    trainer.logger='["swanlab"]'
+    trainer.logger='["swanlab"]' \
+    save_path=../output/Qwen3-8B-SFT-Agentar100K \
