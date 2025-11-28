18ff3d238846dcdc40a7766fceefa65e1d556ebe
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-28 23:37:22 +0800

    post_train_scripts

--- verl/custom/run_qwen_05_sp2_liger.sh
+++ verl/custom/run_qwen_05_sp2_liger.sh
@@ -1,16 +1,5 @@
 set -x
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: run_qwen_05_sp2.sh <nproc_per_node> <save_path> [other_configs...]"
-    exit 1
-fi
-
-nproc_per_node=$1
-save_path=$2
-
-# Shift the arguments so $@ refers to the rest
-shift 2
-
 torchrun --standalone --nnodes=2 --nproc_per_node=$nproc_per_node \
      -m verl.trainer.fsdp_sft_trainer \
     data.train_files=../datasets/Agentar-DeepFinance-100K/train.parquet \
@@ -26,8 +15,7 @@ torchrun --standalone --nnodes=2 --nproc_per_node=$nproc_per_node \
     trainer.default_local_dir=$save_path \
     trainer.project_name=BDI \
     trainer.experiment_name=Qwen3-8B-SFT-Agentar100K \
-    trainer.logger=console $@ \
     ulysses_sequence_parallel_size=2 \
     use_remove_padding=true \
     trainer.logger='["swanlab"]' \
-    save_path=../output/Qwen3-8B-SFT-Agentar100K \
+    save_path=../output/Qwen3-8B-SFT-Agentar100K 
