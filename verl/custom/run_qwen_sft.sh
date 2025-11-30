c8bdf1bb1c68cd12ed2f990df4ee3faf77eb5bb7
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-30 21:49:27 +0800

    sft

--- verl/custom/run_qwen_sft.sh
+++ verl/custom/run_qwen_sft.sh
@@ -1,15 +1,15 @@
 # config文件在verl/verl/trainer/config/sft_trainer.yaml
 
 set -x
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6
 
 PROJECT_NAME="BDI"
-EXPERIMENT_NAME="Qwen3-8B-SFT-Agentar100K"
+EXPERIMENT_NAME="Qwen3-4B-SFT-Agentar100K"
 SAVE_PATH="../output/generator/${PROJECT_NAME}/${EXPERIMENT_NAME}"
 DATA_PATH="../datasets/Agentar-DeepFinance-100K/"
 MODEL_PATH="../pretrain_models/generator/Qwen3-4B"
 
-torchrun --standalone --nnodes=1 --nproc_per_node=2 \
+torchrun --standalone --nnodes=1 --nproc_per_node=6 \
      -m verl.trainer.fsdp_sft_trainer \
     data.train_files=${DATA_PATH}/train.parquet \
     data.val_files=${DATA_PATH}/test.parquet \
