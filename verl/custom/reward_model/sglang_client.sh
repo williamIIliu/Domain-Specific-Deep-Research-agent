8be3dd9639bfe17482d83758527307ab7a47fb1b
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-30 12:49:47 +0800

    reward_model

--- verl/custom/reward_model/sglang_client.sh
+++ verl/custom/reward_model/sglang_client.sh
@@ -5,12 +5,12 @@ python -m sglang.launch_server \
 --trust-remote-code \
 --dtype bfloat16 \
 --served-model-name reward_model \
---max_total_tokens 1024 \
+--max-total-tokens 1024 \
 --tensor-parallel-size 1 \
 --mem-fraction-static 0.20 \
 --api-key sk-123456 \
 --host 0.0.0.0 --port 8060 \
---max_running_requests 4 \
+--max-running-requests 4 \
 --tool-call-parser qwen25 \
 --reasoning-parser qwen3 \
 --context-length 1024 
