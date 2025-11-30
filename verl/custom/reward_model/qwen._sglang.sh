8e983fe49dae0c3cea40d96c68d10c2af4a46ae9
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-30 12:24:57 +0800

    reward_model

--- verl/custom/reward_model/qwen._sglang.sh
+++ verl/custom/reward_model/qwen._sglang.sh
@@ -1,8 +1,9 @@
 # ./pretrain_models/reward_model/FinR1
-CUDA_VISIBLE_DEVICES=1  
+CUDA_VISIBLE_DEVICES=2  
 python -m sglang.launch_server \
 --model-path ./pretrain_models/generator/Qwen3-0.6B \
---trust-remote-code --served-model-name \
+--trust-remote-code \ 
+--served-model-name reward_model \
 --tensor-parallel-size 1 \
 --mem-fraction-static 0.90 \
 --api-key sk-123456 \
