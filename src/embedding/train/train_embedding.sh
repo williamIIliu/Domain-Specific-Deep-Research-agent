fae4b65adbc2211468ca8a63a33d552f7ced1935
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-30 22:14:08 +0800

    finetune embedding

--- src/embedding/train/train_embedding.sh
+++ src/embedding/train/train_embedding.sh
@@ -6,12 +6,17 @@
 export INFONCE_MASK_FAKE_NEGATIVE=true 
 export CUDA_VISIBLE_DEVICES=0,1
 
+PROJECT_NAME="BDI"
+EXPERIMENT_NAME="Qwen3-Embedding-0.6B-Finetune"
+MODEL_PATH="./pretrain_models/embedding/Qwen3-Embedding-0.6B"
+SAVE_PATH="./output/embedding/${PROJECT_NAME}/${EXPERIMENT_NAME}"
+
 # 使用SWIFT_NPROC_PER_NODE启动多卡训练
 NPROC_PER_NODE=2 swift sft \
-    --model ./pretrain_models/embedding/Qwen3-Embedding-0.6B \
+    --model ${MODEL_PATH} \
     --task_type embedding \
     --model_type qwen3_emb \
-    --train_type lora \
+    --train_type full \
     --lora_rank 0 \
     --lora_alpha 32 \
     --lora_dropout 0.05 \
@@ -20,7 +25,7 @@ NPROC_PER_NODE=2 swift sft \
     --max_length 1310 \
     --split_dataset_ratio 0.05 \
     --eval_strategy steps \
-    --output_dir output \
+    --output_dir ${SAVE_PATH} \
     --eval_steps 100 \
     --num_train_epochs 2 \
     --save_steps 100 \
@@ -35,6 +40,6 @@ NPROC_PER_NODE=2 swift sft \
     --deepspeed zero3 \
     --logging_steps 10 \
     --report_to swanlab \
-    --swanlab_project BDI \
-    --swanlab_exp_name Qwen3_emb_0.6B_lora_infonce \
+    --swanlab_project ${PROJECT_NAME} \
+    --swanlab_exp_name ${EXPERIMENT_NAME} \
     --logging_dir ./logs
