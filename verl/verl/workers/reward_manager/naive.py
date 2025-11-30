104174eafa7046be821ac974d8c30cb13f6c4f0f
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-11-30 16:57:38 +0800

    progress reward impleted in VeRL

--- verl/verl/workers/reward_manager/naive.py
+++ verl/verl/workers/reward_manager/naive.py
@@ -43,7 +43,7 @@ class NaiveRewardManager(AbstractRewardManager):
         self.compute_score = compute_score or default_compute_score
         self.reward_fn_key = reward_fn_key  # Store the key for accessing the data source
 
-    def __call__(self, data: DataProto, return_dict: bool = False) -> torch.Tensor | dict[str, Any]:
+    def __call__(self, data: DataProto, return_dict: bool = True) -> torch.Tensor | dict[str, Any]:
         """We will expand this function gradually based on the available datasets"""
 
         # If there is rm score, we directly return rm score. Otherwise, we compute via rm_score_fn
