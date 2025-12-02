c07df303f9d5cbd480721db39a1d01e7da75ac53
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-12-02 20:38:56 +0800

    loading test on the quant model

---
+++ deploy/evalscope/acceptance.sh
@@ -0,0 +1,14 @@
+evalscope perf \
+  --parallel 1 10 50 100 200 \
+  --number 10 20 100 200 400 \
+  --model Fin-Search \
+  --url http://127.0.0.1:11434/v1/chat/completions \
+  --api openai \
+  --dataset openqa \
+  --max-tokens 1024 \
+  --min-tokens 1024 \
+  --prefix-length 0 \
+  --min-prompt-length 1024 \
+  --max-prompt-length 1024 \
+  --tokenizer-path ./pretrain_models/generator/Qwen3-8B/ \
+  --extra-args '{"ignore_eos": true}'
