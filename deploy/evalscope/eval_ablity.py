7bee57b4a731ccee33c0f7facc5838c4557a9f4b
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-12-02 22:50:50 +0800

    eval general ability

---
+++ deploy/evalscope/eval_ablity.py
@@ -0,0 +1,20 @@
+from evalscope.run import run_task
+from evalscope.summarizer import Summarizer
+
+def run_eval():
+    # 选项 1: python 字典
+    # task_cfg = task_cfg_dict
+
+    # 选项 2: yaml 配置文件
+    task_cfg = 'eval_ablility.yaml'
+
+    # 选项 3: json 配置文件
+    # task_cfg = 'eval_openai_api.json'
+
+    run_task(task_cfg=task_cfg)
+
+    print('>> Start to get the report with summarizer ...')
+    report_list = Summarizer.get_report_from_cfg(task_cfg)
+    print(f'\n>> The report list: {report_list}')
+
+run_eval()
