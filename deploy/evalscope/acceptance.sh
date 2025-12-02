cac2565298ca4341573360f4e4eb09497e91b414
LIU Zhiqiang <liu_zq@hust.edu.com>
2025-12-02 22:29:54 +0800

    loading test on the quant model

--- deploy/evalscope/acceptance.sh
+++ deploy/evalscope/acceptance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+export CUDA_VISIBLE_DEVICES=0,1
 evalscope perf \
   --parallel 1 10 50 100 200 \
   --number 10 20 100 200 400 \
@@ -12,7 +12,6 @@ evalscope perf \
   --min-prompt-length 50 \
   --max-prompt-length 1024 \
   --tokenizer-path ./pretrain_models/generator/Qwen3-8B/ \
-  --debug \
   --visualizer swanlab \
   --outputs-dir /tmp/evalscope/Qwen3-8B-Q4_K_M/ \
   --db-commit-interval 5000 
