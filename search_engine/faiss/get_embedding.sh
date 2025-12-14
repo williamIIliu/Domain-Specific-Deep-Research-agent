ea165b4ea0f6519e09b7df78e589cc0a46449e4b
LIU Zhiqiang <LIU Zhiqiang>
2025-12-14 20:28:03 +0800

    embedding for filtered corpus

--- search_engine/faiss/get_embedding.sh
+++ search_engine/faiss/get_embedding.sh
@@ -1,9 +1,9 @@
-python -m torch.distributed.run --nproc_per_node=2 \
+python -m torch.distributed.run --nproc_per_node=7 \
   search_engine/faiss/get_embedding.py \
-  --jsonl_path="./datasets/OmniEval-Corpus/all_data_clean.jsonl" \
+  --jsonl_path="./datasets/OmniEval-Corpus/all_data_clean_new.jsonl" \
   --model_path="./pretrain_models/embedding/Qwen3-Embedding-0.6B" \
   --save_dir="./datasets/database/data_with_embedding_shards" \
   --num_workers=2 \
-  --batch_size=64 \
+  --batch_size=128 \
   --max_length=1024 \
   --fp16
